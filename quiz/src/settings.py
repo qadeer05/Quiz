9f765402d30523f3f7e170bc5956da8cf5b540d2
Qadeer <qadeer05@gmail.com>
2013-11-10 00:39:07 -0500

    Initial Commit

---
+++ quiz/src/settings.py
@@ -0,0 +1,6 @@
+import os.path
+
+
+DEBUG = True
+TEMPLATES_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "templates")
+ASSETS_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "assets")
